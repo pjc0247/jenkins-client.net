12ae0215334e39477afc0a743a1aeb30f6261f87
pjc02478 <pjc0247@gmail.com>
2015-12-09 13:41:34 +0900

    OperationFailedException 추가

---
+++ src/jenkins_client/Exceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsClient
+{
+    public class OperationFailedException : Exception
+    {
+        public string body { get; private set; }
+
+        public OperationFailedException(string msg, string body = "") :
+            base(msg)
+        {
+            this.body = body;
+        }
+    }
+}
