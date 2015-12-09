080bee8b5ac7de6fa7bfcc9e998d7b3829651182
pjc02478 <pjc0247@gmail.com>
2015-12-09 13:41:45 +0900

    update AssemblyInfo

--- src/jenkins_client/Properties/AssemblyInfo.cs
+++ src/jenkins_client/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("jenkins_client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("jenkins client for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("pjc0247")]
 [assembly: AssemblyProduct("jenkins_client")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
